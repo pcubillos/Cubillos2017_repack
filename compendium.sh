4ae609fca35e4c02cfc628e7fb819e552e098b35
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2017-10-03 16:11:46 +0200

    Updated spectrum plot.  Added H2O and NH3 scripts, and pytips.

--- compendium.sh
+++ compendium.sh
@@ -5,24 +5,35 @@ topdir=`pwd`
 cd $topdir
 git clone https://github.com/pcubillos/repack
 cd $topdir/repack
-git checkout 279b1df
+git checkout b3a5474
 make
 
 cd $topdir
 git clone --recursive https://github.com/pcubillos/pyratbay
 cd $topdir/pyratbay
-git checkout 5af22ce
+git checkout 80fd85d
+make
+
+cd $topdir
+git clone https://github.com/pcubillos/pytips
+cd $topdir/pytips
 make
 
 
-# Download Exomol HCN data:
+# Download Exomol data:
 cd $topdir/inputs/
-wget -i wget_exomol_hcn.txt
+wget -i wget_exomol_HCN.txt
+wget -i wget_exomol_NH3.txt
 bzip2 -d *.bz2
 
+# Download HITRAN/HITEMP data:
+cd $topdir/inputs
+wget --user=HITRAN --password=getdata -N -i wget_hitemp_H2O.txt
+unzip '*.zip'
+rm -f *.zip
 
 # Repack HCN line-transition data:
-cd $topdir/run
+cd $topdir/run01
 $topdir/repack/repack.py repack_HCN.cfg
 
 # Format exomol partition functions for PyratBay:
@@ -31,19 +42,30 @@ $topdir/pyratbay/scripts/PFformat_Exomol.py \
        $topdir/inputs/1H-13C-14N__Larner.pf
 
 # Make TLI files for PyratBay:
-cd $topdir/run
+cd $topdir/run01
 $topdir/pyratbay/pbay.py -c tli_HCN_compressed.cfg
 $topdir/pyratbay/pbay.py -c tli_HCN_control.cfg
 
 
 # Figure 1:
 cd $topdir
-$topdir/fig_flagging.py
+python $topdir/fig_flagging.py
 
 # Figure 2:
-cd $topdir/run
-$topdir/fig_hwhm.py
+cd $topdir/run01
+python $topdir/fig_hwhm.py
 
 # Figure 3:
-cd $topdir/run
-$topdir/fig_spectrum.py
+cd $topdir/run01
+python $topdir/fig_spectrum.py
+
+
+# Generate partition-function files for H2O and NH3:
+cd $topdir/run02
+python $topdir/pf_tips_H2O-NH3.py
+
+# Compress LBL databases:
+cd $topdir/run02
+$topdir/repack/repack.py repack_H2O.cfg
+$topdir/repack/repack.py repack_NH3.cfg
+
--- fig_spectrum.py
+++ fig_spectrum.py
@@ -3,6 +3,7 @@
 import sys, os
 import matplotlib
 import numpy as np
+from scipy.ndimage.filters import gaussian_filter1d as gaussf
 import matplotlib.pyplot as plt
 plt.ion()
 
@@ -26,54 +27,68 @@ layer = 95  # 50 bar
 ec3, label3 = pyrat1.get_ec(layer)
 ec4, label4 = pyrat2.get_ec(layer)
 
+# Instrumental resolution (dnu=nu/R):
+dnu = 1.0
+# Sampling rate:
+deltanu = np.abs(np.mean(np.ediff1d(pyrat1.spec.wn)))
+sigma = (dnu/deltanu)/2.355
+# Instrumental down grade:
+efull1 = gaussf(ec1[0], sigma)
+efull3 = gaussf(ec3[0], sigma)
+elbl2  = gaussf(ec2[0], sigma)
+econ2  = gaussf(ec2[1], sigma)
+elbl4  = gaussf(ec4[0], sigma)
+ecom2  = gaussf(ec2[0]+ec2[1], sigma)
+ecom4  = gaussf(ec4[0]+ec4[1], sigma)
+
 lw = 1.0
 xran = 0.55, 33
-fs = 12
+fs = 12  # Fontsize
+t  =  6  # Thinning
 
-plt.figure(-4, (8.5, 8))
+plt.figure(-5, (8.5, 8))
 plt.clf()
 plt.subplots_adjust(0.1, 0.07, 0.97, 0.98, hspace=0.15)
 ax = plt.subplot(311)
-plt.semilogy(wl, ec1[0], lw=lw, color="b", label='Full LBL')
-plt.semilogy(wl, ec2[0], lw=lw, color="r", label='LBL repack')
-plt.semilogy(wl, ec2[1], lw=lw, color="limegreen", label='Continuum repack',
-              alpha=0.8)
+plt.plot(wl[::t], ec1[0,::t], lw=lw, color="b", label='Full LBL')
+plt.plot(wl[::t], ec2[0,::t], lw=lw, color="r", label='Compress LBL')
+plt.plot(wl[::t], ec2[1,::t], lw=lw, color="limegreen",
+         label='Compress continuum', alpha=0.8)
 plt.ylabel("Extinction coefficient (cm-1)")
+ax.set_yscale('log')
 ax.set_xscale('log')
-plt.xlim(xran)
 ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
 ax.set_xticks([0.6, 1.0, 2.0, 5.0, 10.0, 20.0, 30.0])
 ax.set_yticks(np.logspace(-20, -10, 6))
 plt.ylim(1e-20, 3e-10)
-plt.legend(loc="lower right", fontsize=fs)
-# Difference:
-ax = plt.subplot(312)
-plt.plot([0.3, 33.0], [0.0, 0.0], "--", color="0.6")
-plt.plot(wl, 100*(ec2[0]-ec1[0])/ec1[0], ".", ms=2, lw=lw, color="orange")
-plt.plot(wl, 100*(ec4[0]-ec3[0])/ec3[0], ".", ms=2, lw=lw, color="k")
-plt.plot([0,0],[1,1], ".", color="orange", label="0.1 bar")
-plt.plot([0,0],[1,1], ".", color="black",  label="50 bar")
-plt.ylabel("LBL repack $-$ full LBL (%)")
 plt.xlim(xran)
-plt.ylim(-11, 1)
+plt.legend(loc="lower right", fontsize=fs)
+plt.subplots_adjust(0.1, 0.07, 0.97, 0.98, hspace=0.15)
+ax = plt.subplot(312)
+plt.plot(wl[::t], 100*(ecom2/efull1-1)[::t], ".", ms=2, color="orange")
+plt.plot(wl[::t], 100*(elbl2/efull1-1)[::t], ".", ms=2, color="black")
+plt.plot([0,0],[1,1], ".", color="orange", label="LBL+continuum")
+plt.plot([0,0],[1,1], ".", color="black",  label="LBL only")
 ax.set_xscale('log')
 ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
 ax.set_xticks([0.6, 1.0, 2.0, 5.0, 10.0, 20.0, 30.0])
+plt.ylabel("Ext. coeff. difference (%)")
+plt.text(20, -3, "P=0.1 bar")
+plt.ylim(-5, 1)
+plt.xlim(xran)
 plt.legend(loc="lower right", fontsize=fs)
 ax = plt.subplot(313)
-plt.plot([0.3, 33.0], [0.0, 0.0], "--", color="0.6")
-plt.plot(wl, 100*(ec2[0]+ec2[1]-ec1[0])/ec1[0], ".", ms=2, lw=lw,
-         color="orange")
-plt.plot(wl, 100*(ec4[0]+ec4[1]-ec3[0])/ec3[0], ".", ms=2, lw=lw,
-         color="k", zorder=0)
-plt.ylabel(r"LBL+cont repack $-$ full LBL (%)", fontsize=12)
-plt.xlim(xran)
-plt.ylim(-11, 11)
+plt.plot(wl[::t], 100*(ecom4/efull3-1)[::t], ".", ms=2, color="orange")
+plt.plot(wl[::t], 100*(elbl4/efull3-1)[::t], ".", ms=2, color="black")
+plt.ylabel("Ext. coeff. difference (%)")
 ax.set_xscale('log')
 ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
 ax.set_xticks([0.6, 1.0, 2.0, 5.0, 10.0, 20.0, 30.0])
+plt.text(20, -3.5, "P=50 bar")
+plt.ylim(-4, 3)
+plt.xlim(xran)
 plt.xlabel("Wavelength (um)")
 
 plt.savefig("../plots/HCN_spectra.png", dpi=300)
 plt.savefig("../plots/HCN_spectra.ps")
-plt.savefig("../plots/HCN_spectra.pdf")
+#plt.savefig("../plots/HCN_spectra.pdf")
